46466c940ca10058e88e7c4af6ba1d295f3679aa
Duck <st063754@student.spbu.ru>
2021-06-20 15:07:35 +0300

    try2 v3

--- public/script.js
+++ public/script.js
@@ -4,41 +4,31 @@ var io = io.connect('https://st063754-whiteboard.herokuapp.com/')
 //var io = io.connect('localhost:8080')
 
 let current = {x: 0, y: 0}
-let last = {x: 0, y: 0}
+let lastMy = {x: 0, y: 0}
 let lastOther = {x: 0, y: 0}
+
+let last = [lastMy, lastOther]
 
 let mouseDown = false
 
 
-function Draw(x, y, color, width) {
+function Draw(x, y, color, width, prev) {
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.lineWidth = width;
     ctx.lineJoin = "round";
-    ctx.moveTo(last.x, last.y);
+    ctx.moveTo(prev.x, prev.y);
     ctx.lineTo(x, y);
     ctx.closePath();
     ctx.stroke();
-    last.x = x;
-    last.y = y;
-}
-function DrawOther(x, y, color, width) {
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width;
-    ctx.lineJoin = "round";
-    ctx.moveTo(lastOther.x, lastOther.y);
-    ctx.lineTo(x, y);
-    ctx.closePath();
-    ctx.stroke();
-    lastOther.x = x;
-    lastOther.y = y;
+    prev.x = x;
+    prev.y = y;
 }
 
 function init(){
     window.onmouseup = (e) => { mouseDown = false }
     canvas.onmousedown = (e) => {
-        last = Object.assign({}, current)
+        last[0] = Object.assign({}, current)
         ctx.moveTo(current.x,current.y)
         io.emit('down', {x: current.x, y: current.y})
         mouseDown = true
@@ -49,14 +39,14 @@ function init(){
         if (mouseDown) {
             let c = document.getElementById("color").value;
             let w = document.getElementById("width").value;
-            Draw(current.x, current.y, c, w)
+            Draw(current.x, current.y, c, w, last[0])
             io.emit('draw', {x: current.x, y: current.y, c: c, w: w})
         }
     }
 }
 
 
-io.on('ondraw', ({x,y,c,w}) => { DrawOther(x,y,c,w) })
-io.on('ondown', ({x,y}) => { lastOther = {x,y} })
+io.on('ondraw', ({x,y,c,w}) => { Draw(x,y,c,w, last[1]) })
+io.on('ondown', ({x,y}) => { last[1] = {x,y} })
 
 
